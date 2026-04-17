d194100218f00c9274b3c158ce62130b774886ea
hangrybear666 <hangrybear666+github@proton.me>
2026-04-17 10:56:49 +0200

    Adds http external targets in loop

--- cmd/healthcheck/main.go
+++ cmd/healthcheck/main.go
@@ -16,31 +16,30 @@ func main() {
 		log.Fatalf("Targets could not be loaded from conf: %v", err)
 	}
 
+	ctx := context.Background() // TODO: replace with server http request context later
+
 	// Acquire request targets from yaml
 	var target *config.Target
 	for _, t := range conf.Targets.External {
-		if t.Name == "Demo Backend" {
+		if t.Type == "http" {
 			target = &t
-			fmt.Printf("Target acquired %v\n", target)
-			break
+			fmt.Printf("http target acquired [%v]\n ===> Sending http query en route [%v]\n", target.Name, target.URL)
+			// send http requests to targets
+			client := requests.CreateClient(conf.GlobalTimeout)
+			result := client.QueryHttp(ctx, target)
+			if target.X509Verify {
+				certValid := client.VerifyTLSCertificate(ctx, target, conf.RootDomain)
+				fmt.Printf("%v\n", certValid)
+			}
+
+			// ASCII format result from request
+			response := responses.ASCII([]requests.Result{result})
+			fmt.Printf("%v\n", response)
 		}
 	}
 	if target == nil {
 		panic("Could not acquire target from conf.")
 	}
-
-	// send http requests to targets
-	ctx := context.Background() // TODO: replace with server http request context later
-	client := requests.CreateClient(conf.GlobalTimeout)
-	result := client.QueryHttp(ctx, target)
-	if target.X509Verify {
-		certValid := client.VerifyTLSCertificate(ctx, target, conf.RootDomain)
-		fmt.Printf("%v\n", certValid)
-	}
-
-	// ASCII format result from request
-	response := responses.ASCII([]requests.Result{result})
-	fmt.Printf("%v\n", response)
 
 	return
 }
