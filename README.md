513bc532174d68f4a86af79da1e1c2ae877bc311
hangrybear666 <hangrybear666+github@proton.me>
2026-04-18 12:10:22 +0200

    Refactors go deployment script

--- README.md
+++ README.md
@@ -2,32 +2,37 @@
 
 Unified Monitoring Endpoint written in Go, concurrently requesting health and status information from all available Fiscalismia Resources to output ASCII formatted data formatted for CLI client queries via e.g. curl from remote devices such as mobile phones, allowing an unified view of system landscape health.
 
-## Setup
+## Golang Healthcheck
+### Setup
 
 ```bash
 # Install go on fedora
 sudo dnf install -y --quiet golang
-cd ~/git/fiscalismia-monitoring/
+cd ~/git/fiscalismia-monitoring/golang
 go mod tidy
 ```
 
-## Running
+### Running
 
+**Naively**
 ```bash
 go run ./cmd/healthcheck/
 ```
 
-## Updating
-
-
-## Testing
-
+**Properly**
 ```bash
 gofmt -s -w . && go vet ./...
 go build ./cmd/healthcheck/
 ./healthcheck
-
 ```
+
+### Updating
+### Testing
+
+## Prometheus & Grafana
+
+### Setup
+### Updating
 
 ## License
 
---
+++ scripts/deploy_golang_dev_binary.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR=$(pwd | sed 's:.*/::')
+if [ "$CURRENT_DIR" != "scripts" ]
+then
+  log_msg "please change directory to scripts folder and execute the shell script again."
+  exit 1
+fi
+
+readonly BRIGHT_GREEN=$'\033[92m'
+readonly CYAN=$'\033[36m'
+readonly MAGENTA=$'\033[35m'
+readonly RESET=$'\033[0m'
+
+log_msg () {
+  printf "${CYAN}===>${MAGENTA} $1${RESET}\n"
+}
+
+cd ../golang/
+
+log_msg "formatting go files"
+gofmt -s -w .
+
+log_msg "vetting go files"
+go vet ./...
+
+log_msg "building go binary"
+go build ./cmd/healthcheck/
+
+log_msg "creating remote project structure"
+ssh loadbalancer mkdir -p /usr/local/etc/go
+
+log_msg "copying binary and target config to remote"
+scp ./healthcheck ./targets.yml root@loadbalancer:/usr/local/etc/go/
+
+log_msg "${BRIGHT_GREEN}EXECUTING BINARY ON REMOTE"
+ssh loadbalancer << 'EOF'
+cd /usr/local/etc/go/
+./healthcheck
+exit 0
+EOF
