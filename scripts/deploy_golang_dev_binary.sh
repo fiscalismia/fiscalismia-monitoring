6f2abe6b8fbb078aaf16c611195df80f84706980
hangrybear666 <hangrybear666+github@proton.me>
2026-04-19 21:37:35 +0200

    updates deployment script for go compose

--- scripts/deploy_golang_dev_binary.sh
+++ scripts/deploy_golang_dev_binary.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR=$(pwd | sed 's:.*/::')
-if [ "$CURRENT_DIR" != "scripts" ]
-then
-  log_msg "please change directory to scripts folder and execute the shell script again."
-  exit 1
-fi
-
 readonly BRIGHT_GREEN=$'\033[92m'
 readonly CYAN=$'\033[36m'
 readonly MAGENTA=$'\033[35m'
@@ -16,27 +9,13 @@ log_msg () {
   printf "${CYAN}===>${MAGENTA} $1${RESET}\n"
 }
 
-cd ../golang/
-
-log_msg "formatting go files"
-make fmt
-
-log_msg "vetting go files"
-make vet
-
-log_msg "building go binary"
-make clean
-make build
-
-log_msg "creating remote project structure"
-ssh loadbalancer mkdir -p /usr/local/etc/go
-
-log_msg "copying binary and target config to remote"
-scp ./healthcheck ./targets.yml root@loadbalancer:/usr/local/etc/go/
-
-log_msg "${BRIGHT_GREEN}EXECUTING BINARY ON REMOTE"
-ssh loadbalancer << 'EOF'
-cd /usr/local/etc/go/
-./healthcheck
-exit 0
+log_msg "Establishing ssh connection to remote."
+ssh demo << 'EOF'
+  echo stopping running container
+  docker-compose stop fiscalismia-healthcheck || true
+  echo launching new container
+  docker compose up --no-deps fiscalismia-healthcheck -d
+  sleep 5
+  echo checking container logs
+  podman logs --follow fiscalismia-healthcheck
 EOF
